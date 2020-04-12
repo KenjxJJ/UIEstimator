e8b65e52abd4a1b6f34b8033e1f47982b146d32a
JOEL KIGGWE <joelkiggwe94@gmail.com>
2020-04-12 22:56:22 +0300

    UI Estimator Build

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('NGO-web app is running!');
+    expect(page.getTitleText()).toEqual('uiestimator app is running!');
   });
 
   afterEach(async () => {
